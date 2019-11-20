1246a1985c18fb8304d2e5f576e6ea5865ae3db3
Alina Zhukouskaya <laymalay@gmail.com>
2019-11-20 15:05:09 +0300

    added run script

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ tmux -2 new-session -d -s $SESSION
 tmux new-window -t $SESSION:1 -n 'Polls'
 tmux split-window -h
 tmux select-pane -t 0
-tmux send-keys "cd polls_api; ./deploy.sh" C-m
+tmux send-keys "cd polls_api; source venv/bin/activate; manage.py runserver" C-m
 tmux select-pane -t 1
-tmux send-keys "cd ui; ./deploy.sh" C-m
+tmux send-keys "cd ui; npm start" C-m
 tmux split-window -v
-tmux resize-pane -D 40
+tmux resize-pane -D 60
 
 tmux -2 attach-session -t $SESSION
